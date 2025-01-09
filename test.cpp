f8ba92b4a5ddb86e5c14c38d5291ae2190c1acb4
Zee Bunny <zeebunny3@gmail.com>
2025-01-09 16:55:00 -0500

    added test.exe

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Name: ";
+    cin >> name;
+    cout << "Age: ";
+    cin >> age;
+    cout << name << " is " << age << " years old";
+}
